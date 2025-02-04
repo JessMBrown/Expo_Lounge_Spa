ca303b1875ec50cb776cb42df641510d34f3bd7a
JessMBrown <160969224+JessMBrown@users.noreply.github.com>
2025-02-04 09:26:08 +0000

    Create bookings_table.py

---
+++ bookings_table.py
@@ -0,0 +1,26 @@
+bookings = [
+    {"Spa_Booking_ID": '00001',
+     "Date_of_Treatment": "2024-06-01",
+     "Time_of_Treatment": "10:30:00",
+     "Treatment_ID": 100,
+     "Employee_ID": 100},
+
+    {"Spa_Booking_ID": "00002",
+     "Date_of_Treatment": "2024-06-01",
+     "Time_of_Treatment": "11:30:00",
+     "Treatment_ID": 100,
+     "Employee_ID": 100},
+
+    {"Spa_Booking_ID": "00003",
+     "Date_of_Treatment": "2024-06-01",
+     "Time_of_Treatment": "13:30:00",
+     "Treatment_ID": 100,
+     "Employee_ID": 100},
+
+    {"Spa_Booking_ID": "00004",
+     "Date_of_Treatment": "2024-06-02",
+     "Time_of_Treatment": "13:45:00",
+     "Treatment_ID": 100,
+     "Employee_ID": 100}
+
+]
