ba4393321d3b5e4f0284bd7293141acbf689e3b5
JessMBrown <160969224+JessMBrown@users.noreply.github.com>
2025-02-04 09:28:57 +0000

    Create employee_table.py

---
+++ employee_table.py
@@ -0,0 +1,23 @@
+employees = [
+    {"employee_ID": "0001",
+     "role": "Masseuse",
+     "salary": 35000.00,
+     "work_days": ["WED, THU, FRI, SAT, SUN"],
+     "start_time": "06:00:00",
+     "finish_time": "14:00:00"},
+
+    {"employee_ID": "0002",
+     "role": "Beautician",
+     "salary": 39000.00,
+     "work_days": ["MON, TUE, WED, THU, FRI"],
+     "start_time": "06:00:00",
+     "finish_time": "14:00:00"},
+
+    {"employee_ID": "0003",
+     "role": "Nail_Technician",
+     "salary": 30000.00,
+     "work_days": ["MON, TUE, SAT, SUN"],
+     "start_time": "06:00:00",
+     "finish_time": "14:00:00"},
+
+]
